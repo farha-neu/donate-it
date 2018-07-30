6ff8a382aa542522abdeecb17f81bb860c26f750
farha-neu <farhajw@gmail.com>
2018-07-29 20:19:27 -0700

    signup form

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -5,35 +5,47 @@ import axios from "axios";
 
 
 class Signup extends React.Component{
-
     state={
+        firstname:"",
+        lastname:"",
         username:"",
         email:"",
         password:"",
-        phonenumber:""
+        phonenumber:"",
+        city:"",
+        state:"",
+        zipcode:""
     }
 
     handleInputChange = event => {
         const name = event.target.name;
         const value = event.target.value;
         this.setState({
-          [name]: value // name is search
+          [name]: value 
         });
     };
 
     handleFormSubmit = event =>{
         event.preventDefault();
-        if(this.state.username!=="" && this.state.email!=="" && this.state.password!=="" && this.state.phonenumber!==""){
-            axios.post("/signup",{username:this.state.username, 
+        //all fields are required...need to add error message
+        if(this.state.firstname!=="" && this.state.lastname!=="" &&
+        this.state.username!=="" && this.state.email!=="" && this.state.password!=="" && this.state.phonenumber!==""
+         && this.state.city!=="" && this.state.state!=="" && this.state.zipcode!==""){
+            axios.post("/signup",{firstname:this.state.firstname,
+                                  lastname:this.state.lastname,
+                                  username:this.state.username, 
                                   email:this.state.email, 
                                   password:this.state.password, 
-                                  phonenumber:this.state.phonenumber}).then(response=>{
-                                      console.log(response);
-                                      this.props.history.push("/login");
-                                    //   <Redirect to="/login" />
-
-        })
+                                  phonenumber:this.state.phonenumber,
+                                  city:this.state.city,
+                                  state:this.state.state,
+                                  zipcode:this.state.zipcode}).then(response=>{
+                                  console.log(response);
+                                  this.props.history.push("/login");
+        
+          })
         }
+    
     }
 
     render(){
@@ -45,32 +57,67 @@ class Signup extends React.Component{
                 <span className="donate"> DoNATE </span>-  iT!
             </div>
             <input className="loginInput"
+                value={this.state.firstname}
+                name="firstname"
+                onChange={this.handleInputChange}
+                type="text"
+                placeholder="First name*"
+            />
+             <input className="loginInput"
+                value={this.state.lastname}
+                name="lastname"
+                onChange={this.handleInputChange}
+                type="text"
+                placeholder="Last name*"
+            />
+            <input className="loginInput"
                 value={this.state.username}
                 name="username"
                 onChange={this.handleInputChange}
                 type="text"
-                placeholder="Username"
+                placeholder="Username*"
             />
             <input className="loginInput"
                 value={this.state.password}
                 name="password"
                 onChange={this.handleInputChange}
                 type="password"
-                placeholder="Password"
+                placeholder="Password*"
             />
             <input className="loginInput"
                 value={this.state.email}
                 name="email"
                 onChange={this.handleInputChange}
                 type="email"
-                placeholder="Email"
+                placeholder="Email*"
+            />
+             <input className="loginInput"
+               value={this.state.city}
+                name="city"
+                onChange={this.handleInputChange}
+                type="text"
+                placeholder="City*"
+            />
+             <input className="loginInput"
+                value={this.state.state}
+                name="state"
+                onChange={this.handleInputChange}
+                type="text"
+                placeholder="State*"
+            />
+             <input className="loginInput"
+                value={this.state.zipcode}
+                name="zipcode"
+                onChange={this.handleInputChange}
+                type="text"
+                placeholder="Zipcode*"
             />
             <input className="loginInput"
                 value={this.state.phonenumber}
                 name="phonenumber"
                 onChange={this.handleInputChange}
                 type="text"
-                placeholder="Phone number"
+                placeholder="Phone number*"
             />
             <button className="signUpButton" onClick={this.handleFormSubmit}>Submit</button>
 
